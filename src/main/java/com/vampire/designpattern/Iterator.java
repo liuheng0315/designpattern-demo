caefcd2931faeb94aa16d072e8efd425e09faa77
liuheng <liuheng0315@gmail.com>
2021-03-30 23:52:13 +0800

    迭代器模式

--- src/main/java/com/vampire/designpattern/Iterator.java
+++
@@ -1,7 +0,0 @@
-package com.vampire.designpattern;
-
-/**
- * @author ASUA
- */
-public class Iterator {
-}
---
+++ src/main/java/com/vampire/designpattern/iterator/Student.java
@@ -0,0 +1,16 @@
+package com.vampire.designpattern.iterator;
+
+/**
+ * @author ASUA
+ */
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
